0edefc1462b085f08044f03eeaef72ff97a7b53d
Ashish Dhiwar <112108089+Ashishxo@users.noreply.github.com>
2025-08-11 20:40:56 +0530

    testing ci/cd

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
           <div className='flex flex-col justify-center w-[45%]'>
             <div className='w-full bg-[rgb(22,27,34)] px-5 py-5 rounded-3xl shadow-2xl'> 
               <input readOnly value={password} placeholder='password' ref={passRef} type="text" className='bg-[rgb(65,71,78)] px-5 py-2 inset-x-0 w-5/6 inline rounded-l-3xl text-white outline-none shadow-xl' />
-              <button className='duration-200 px-3 py-2 rounded-r-3xl bg-gradient-to-r from-[#8A2387] via-[#E94057] to-[#F27121] text-[#ccc] hover:from-[#b02aa9] hover:via-[#f73c55] hover:to-[#ff8b43] shadow-xl w-1/6  active:from-[#8A2387] active:via-[#E94057] active:to-[#F27121]' onClick={copyToClipboard}>Copy</button>
+              <button className='duration-200 px-3 py-2 rounded-r-3xl bg-gradient-to-r from-[#8A2387] via-[#E94057] to-[#F27121] text-[#ccc] hover:from-[#b02aa9] hover:via-[#f73c55] hover:to-[#ff8b43] shadow-xl w-1/6  active:from-[#8A2387] active:via-[#E94057] active:to-[#F27121]' onClick={copyToClipboard}>Chopy</button>
 
               <input type="range" className='mt-8 ml-4 bg-[rgb(65,71,78)] h-2 rounded-full accent-purple-600 cursor-pointer' style={{WebkitAppearance:"none"}} min={6} max={25} value={length} onChange={(e)=>{setLength(parseInt(e.target.value))}}/>
               <label className='text-white ml-3'>Length: {length}</label>
